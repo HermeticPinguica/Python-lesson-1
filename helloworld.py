9a287c9fc8970b1142a11e5e965a90352cd96d7e
HermeticPinguica <luis.ballestereyes@gmail.com>
2021-11-02 16:03:23 -0600

    numeros pares

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,15 @@
 var = 'mundo'
 print (var)
+x= 30.5
+y= 60.5
+print (x+y)
+
+if not False:
+    print ('La negacion es falsa')
+
+i=0
+while i<=100:
+    if (i%2 == 0):
+        print (i, end = ',')
+    i+=1
+    
